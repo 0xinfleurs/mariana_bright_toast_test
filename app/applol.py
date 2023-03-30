50ce2fdb3af64a558ec1ed0696e8b6b4fefbf1d8
Mariana Herrera <marianafleurs@gmail.com>
2023-03-30 15:30:19 -0600

    11th commit, emoji test

--- app/applol.py
+++ app/applol.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.set_page_config(layout='wide')
-st.image('./header.png')
-st.title("prediction for lol")
-st.text("predicts the winner based on team and champions")
+#st.image('./header.png')
+st.title("ML model for Esports betting")
+st.text("This amazing machine learning model predicts the probability to win a game based upon each element of the team and the champions they're using")
 
-team_name = st.text_imput('What s )
+team_name = st.text_imput('Tell us about your team')
+champions = st.multiselect('Select a champion',[ 'champ1', 'champ2', 'champ3'])
